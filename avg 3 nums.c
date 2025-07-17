039ee1e7bedf29def5c6b88b87b3756a96ec07b4
AHR144 <ahonufrafi144@gmail.com>
2025-07-17 16:47:20 +0600

    Add files via upload

---
+++ avg 3 nums.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int x,y,z;
+
+    printf( "enter 3 numbers: ");
+    
+        scanf("%d %d %d",&x,&y,&z);
+       
+    
+    int avg = (x+y+z)/3;
+
+    printf("average is :%d\n",avg);
+    
+}
